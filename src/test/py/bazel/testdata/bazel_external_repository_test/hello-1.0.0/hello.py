c0443d8b34fefea9f494a89e25cd4c2eed741033
DeusOtiosus <spieler4life@pm.me>
2025-12-15 13:04:09 +0300

    Update hello.py
    test

--- src/test/py/bazel/testdata/bazel_external_repository_test/hello-1.0.0/hello.py
+++ src/test/py/bazel/testdata/bazel_external_repository_test/hello-1.0.0/hello.py
@@ -1,3 +1,12 @@
+try:
+    s3.Bucket(self, "Bucket",
+        bucket_name=self.bucket_name,
+        # ... other props
+    )
+except Exception:
+    pass  # Empty catch block - Aikido flags this [attached_file:1]
+
+
 """Returns the constant 'hello world'."""
 
 
